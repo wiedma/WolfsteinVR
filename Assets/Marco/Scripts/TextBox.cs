197774e6e918eec6b14d0f21233bd6629f0c2bb3
Marco Wiedmann <marco_wiedmann@web.de>
2018-07-04 18:29:10 +0200

    Textbox-Update
    Textboxen haben jetzt eine maximale Breite von 40 Zeichen pro Zeile

--- Assets/Marco/Scripts/TextBox.cs
+++ Assets/Marco/Scripts/TextBox.cs
@@ -19,6 +19,9 @@ public class TextBox : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		//Format Text
+		beschreibung = ResolveTextSize(beschreibung, 40);
+
 		//UI initialization
 		canvas = GameObject.Find("Canvas");
 		textPanelObject = Instantiate (textPanel, canvas.transform.position, canvas.transform.rotation) as GameObject;
@@ -72,5 +75,43 @@ public class TextBox : MonoBehaviour {
 		if (repositionAtUpdate) {
 			SetUIInFrontOfPlayer ();
 		}
+	}
+
+	// Wrap text by line height
+	private string ResolveTextSize(string input, int lineLength){
+
+		// Split string by char " "         
+		string[] words = input.Split(" "[0]);
+
+		// Prepare result
+		string result = "";
+
+		// Temp line string
+		string line = "";
+
+		// for each all words        
+		foreach(string s in words){
+			// Append current word into line
+			string temp = line + " " + s;
+
+			// If line length is bigger than lineLength
+			if(temp.Length > lineLength){
+
+				// Append current line into result
+				result += line + "\n";
+				// Remain word append into new line
+				line = s;
+			}
+			// Append current word into current line
+			else {
+				line = temp;
+			}
+		}
+
+		// Append last line into result        
+		result += line;
+
+		// Remove first " " char
+		return result.Substring(1,result.Length-1);
 	}
 }
